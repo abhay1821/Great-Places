80b3255361c27b43d74027439af3552f83a6f151
Abhay Chaudhary <chaudharyabhay111@gmail.com>
2021-09-15 23:34:33 +0530

    google map

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,27 +1,27 @@
 import 'dart:io'; //file
 
 class PlaceLocation {
-  final double latitude;
-  final double longitude;
-  final String address;
+  final double? latitude;
+  final double? longitude;
+  final String? address;
 
-  PlaceLocation({
-    required this.latitude,
-    required this.longitude,
-    this.address = "",
+  const PlaceLocation({
+    this.latitude,
+    this.longitude,
+    this.address,
   });
 }
 
 class Place {
   final String id;
   final String title;
-  final PlaceLocation location;
+  final PlaceLocation? location;
   final File image;
 
   Place({
     required this.id,
     required this.title,
-    required this.location,
+    this.location,
     required this.image,
   });
 }
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+  //getting the coordinates of the location
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: locData.latitude,
+      longitude: locData.longitude,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      //creting the new route on fly
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      //..
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              border: Border.all(
+            width: 1,
+            color: Colors.grey,
+          )),
+          child: _previewImageUrl!.isEmpty
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(Icons.location_on),
+              label: Text('Current location'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              onPressed: _selectOnMap,
+              icon: Icon(Icons.map),
+              label: Text('Select On map'),
+              textColor: Theme.of(context).primaryColor,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
